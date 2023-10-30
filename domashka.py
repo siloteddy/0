ad6eba7b57441d78ae11a7d3afe140969b0e90e0
shilorya <doknes@mail.ru>
2023-10-30 20:19:20 +0300

    1

--- domashka.py
+++ domashka.py
@@ -16,22 +16,62 @@ files = line[2]
 # сколько файлов в папке в имени имеют 2 буквы g подряд?
 # сколько файлов в папке в имени имеют сумму чисел больше 10?
 
-import random
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-nums = '1234567890'
+# task 1
+print(len(files))
 
-for i in range(500):
-    has_nums = random.randint(1,10) > 7  
+# task 2
+count = 0
+for i in files:
+    for j in '1234567890':
+        if j in i:
+            count += 1
+            break
+print(count)
 
-    name = folder
+# task 3
+count = 0
+for i in files:
+    if 'g' in i:
+        count += 1
+        continue
+print(count)
 
-    for i in range(random.randint(5,40)):
-        name += random.choice(alphabet)
-        if has_nums and random.randint(1,10) > 7 :
-            name += random.choice(nums)   
+# task 4
+count = 0
+for i in files:
+    if 'gg' in i:
+        count += 1
+        continue
+print(count)
 
-    name += '.txt' 
+# task 5
+# count = 0
+# for i in files:
+
+
+
+
+
+
+
+
+
+# import random
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# nums = '1234567890'
+
+# for i in range(500):
+#     has_nums = random.randint(1,10) > 7  
+
+#     name = folder
+
+#     for i in range(random.randint(5,40)):
+#         name += random.choice(alphabet)
+#         if has_nums and random.randint(1,10) > 7 :
+#             name += random.choice(nums)   
+
+#     name += '.txt' 
    
-    with open(name, 'w', encoding='utf8') as f:
-        f.write('lol')
+#     with open(name, 'w', encoding='utf8') as f:
+#         f.write('lol')
 
